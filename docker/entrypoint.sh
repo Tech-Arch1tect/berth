b076d3eb785f67ff2dc441fcc02adc251111330f
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-07-23 00:10:43 +0100

    Create required laravel storage directories at startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+mkdir -p /var/www/html/storage/framework/sessions \
+         /var/www/html/storage/framework/views \
+         /var/www/html/storage/framework/cache
 [ -d "/var/www/html/storage" ] && chown -R appuser:appgroup /var/www/html/storage
 [ -f "/var/www/html/database/database.sqlite" ] && chown appuser:appgroup /var/www/html/database/database.sqlite
 
